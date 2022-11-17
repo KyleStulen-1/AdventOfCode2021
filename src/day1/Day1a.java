28d07e63c82e909da0a52b72e6a91b5f3dfc03bd
KyleStulen-1 <62697063+KyleStulen-1@users.noreply.github.com>
2022-11-16 23:48:04 -0600

    update day1

--- src/day1/Day1a.java
+++ src/day1/Day1a.java
@@ -4,27 +4,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import operations.GeneralStreamOperations;
+import operations.StringStreamOperations;
 
 public class Day1a {
 
 	public static void main(String[] args) {
-		List<Integer> waterDepths = null;
-		try {
-			waterDepths = readStringStreamFromFileToIntegerArray("day1a.txt");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("unable to read file");
-			System.exit(0);
-		}
+		Stream<String> input = null;
+		String inputFileName = "day1a.txt";
+		
+		input = readStringStreamFromFile(inputFileName);
+
+		//////
+		Function<Stream<String>,Stream<Integer>> func = StringStreamOperations::toInteger;
+		
+		Function<Stream<Integer>,List<Integer>> func2 = GeneralStreamOperations::toList;
+		
+		List<Integer> waterDepths = func.andThen(func2).apply(input);
+		///
 		
 		int depthsGreaterThanPreviousDepths = countGreaterThanPreviousInList(waterDepths);
 		
 		System.out.println(depthsGreaterThanPreviousDepths);
 	}
 	
-	static List<Integer> readStringStreamFromFileToIntegerArray(String fileName) throws IOException {
-		return Files.lines(Path.of(System.getProperty("user.dir"), "src", "inputs", fileName)).map(Integer::valueOf).collect(Collectors.toList());
+	static Stream<String> readStringStreamFromFile(String fileName) {
+		Stream<String> stream = null;
+		try {
+			stream = Files.lines(Path.of(System.getProperty("user.dir"), "src", "inputs", fileName));
+		} catch (IOException e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+		return stream;
 	}
 	
 	static int countGreaterThanPreviousInList(List<Integer> list) {
